14a8680758ad90f54243143916fff49796cb8d5c
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-16 20:50:40 -0300

    [fix] Inventory.import_data too complex

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -6,19 +6,25 @@ from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
 
+def csv_decode(file):
+    result = []
+    header, *data = csv.reader(file, delimiter=",", quotechar='"')
+    for i in data:
+        ob = {}
+        for j in range(len(header)):
+            ob[header[j]] = i[j]
+        result.append(ob)
+    return result
+
+
 class Inventory:
     @staticmethod
     def import_data(arq, tipo):
         result = []
         with open(arq, encoding="utf8") as file:
-            if '.csv' in arq:
-                header, *data = csv.reader(file, delimiter=",", quotechar='"')
-                for i in data:
-                    ob = {}
-                    for j in range(len(header)):
-                        ob[header[j]] = i[j]
-                    result.append(ob)
-            elif '.json' in arq:
+            if ".csv" in arq:
+                result = csv_decode(file)
+            elif ".json" in arq:
                 result = json.load(file)
 
         if tipo == "simples":
