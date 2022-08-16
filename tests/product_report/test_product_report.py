1d1ea2e12e07a528d61dbfc84bec24227ee4a48b
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-16 16:35:19 -0300

    [test] requisito 2 ok

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,18 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    assert (
+        Product(
+            0,  # id
+            "farinha",  # nome_do_produto
+            "Farinini",  # nome_da_empresa
+            "01-05-2021",  # data_de_fabricacao
+            "02-06-2023",  # data_de_validade
+            "12345678",  # numero_de_serie
+            "ao abrigo de luz",  # instrucoes_de_armazenamento
+        ).__repr__()
+        == "\
+O produto farinha fabricado em 01-05-2021 por Farinini com validade \
+até 02-06-2023 precisa ser armazenado ao abrigo de luz."
+    )
