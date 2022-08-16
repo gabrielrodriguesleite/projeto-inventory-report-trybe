f772ddc6e008b0359d6d6acd3f6afbc5d4b2fafc
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-16 16:20:57 -0300

    [test] requisito 1 ok

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,18 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    assert (
+        Product(
+            0,  # id
+            "produto nome",  # nome_do_produto
+            "empresa nome",  # nome_da_empresa
+            "2022-02-22",  # data_de_fabricacao
+            "2023-03-23",  # data_de_validade
+            "12345678",  # numero_de_serie
+            "Armazenar em local ventilado",  # instrucoes_de_armazenamento
+        ).__repr__()
+        == "\
+O produto produto nome fabricado em 2022-02-22 por empresa nome com validade \
+até 2023-03-23 precisa ser armazenado Armazenar em local ventilado."
+    )
